ef2d3e3d597c02adfc6bceb385cb33efc9e2fc3f
Chisato <hln0x29a@gmail.com>
2025-09-15 16:52:26 +0800

    1. lab2b tried but failed

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,10 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"log"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -9,8 +12,10 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
-	// You may add code here
+	ck   kvtest.IKVClerk
+	lkey string
+	lsta string
+	ver  rpc.Tversion
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +24,42 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
-	// You may add code here
+	lk := &Lock{ck: ck, lkey: kvtest.RandValue(8), lsta: l, ver: 0}
+	_, ver, gerr := lk.ck.Get(l)
+	if gerr == rpc.ErrNoKey {
+		perr := lk.ck.Put(lk.lkey, "", ver)
+		if perr != rpc.OK {
+			log.Fatalf("MakeLock Bug, putRpcErr: %s", perr)
+		}
+		lk.ver += 1
+	} else {
+		lk.ver = ver
+	}
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Your code here
+	for lsta, ver, gerr := lk.ck.Get(lk.lkey); ; {
+		if gerr != rpc.OK {
+			log.Fatalf("Lock Acquire Bug, getRpcErr: %s", gerr)
+		} else if lsta == lk.lsta { // locked retry
+			lk.ver = ver
+			continue
+		} else if len(lsta) == 0 { // lock free
+			lk.ver = ver
+			lk.ck.Put(lk.lkey, lk.lsta, ver)
+			break
+		}
+	}
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	if lsta, ver, gerr := lk.ck.Get(lk.lkey); gerr != rpc.OK {
+		log.Fatalf("Lock Release Bug, getRpcErr: %s", gerr)
+	} else if lsta == lk.lsta {
+		lk.ver = ver
+		lk.ck.Put(lk.lkey, "", ver)
+	} else {
+		lk.ver = ver
+	}
 }
